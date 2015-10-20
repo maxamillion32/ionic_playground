c8aff79e8384884f25230e93e9875f5f1fa16cd9
Nathan Lyle Black <lyleblack@gmail.com>
2015-10-19 19:02:55 -0700

    add badges

--- ionic-course/www/js/controllers.js
+++ ionic-course/www/js/controllers.js
@@ -64,10 +64,15 @@ Controller for the discover page
 /*
 Controller for the favorites page
 */
-.controller('FavoritesCtrl', function($scope, User) {
+.controller('FavoritesCtrl', function($scope, User, $window) {
   $scope.favorites = User.favorites;
+
   $scope.removeSong = function(song, index) {
     User.removeSongFromFavorites(song, index);
+  };
+
+  $scope.openSong = function(song) {
+    $window.open(song.open_url, "_system");
   };
 
 })
@@ -76,12 +81,15 @@ Controller for the favorites page
 /*
 Controller for our tab bar
 */
-.controller('TabsCtrl', function($scope) {
+.controller('TabsCtrl', function($scope, User, Recommendations) {
   $scope.enteringFavorites = function() {
+    User.newFavorites = 0;
     Recommendations.haltAudio();
   };
 
   $scope.leavingFavorites = function() {
     Recommendations.init();
   };
+
+  $scope.favCount = User.favoriteCount;
 });
