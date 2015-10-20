c58b55899bafd15fd83d5dd4c05387929e99b9c2
Nathan Lyle Black <lyleblack@gmail.com>
2015-10-19 18:56:29 -0700

    halfway through ionic-course tutorial

---
+++ ionic-course/www/js/controllers.js
@@ -0,0 +1,87 @@
+angular.module('songhop.controllers', ['ionic', 'songhop.services'])
+
+
+/*
+Controller for the discover page
+*/
+.controller('DiscoverCtrl', function($scope, $ionicLoading, $timeout, User, Recommendations) {
+
+  var showLoading = function() {
+    $ionicLoading.show({
+      template: 'i class="ion-loading-c"></i>',
+      noBackdrop: true
+    });
+  }
+
+  var hideLoading = funciton() {
+    $ionicLoading.hide();
+  };
+
+  showLoading();
+
+  Recommendations.init()
+    .then(function() {
+      $scope.currentSong = Recommendations.queue[0];
+    //  return Recommendations.playCurrentSong();
+    })
+    .then(function(){
+      hideLoading();
+      $scope.currentSong.loaded = true;
+    });
+
+  $scope.sendFeedback = function(bool) {
+
+    if (bool) User.addSongToFavorites($scope.currentSong);
+
+    $scope.currentSong.rated = bool;
+    $scope.currentSong.hide = true;
+
+    Recommendations.nextSong();
+
+    $timeout(function() {
+      $scope.currentSong = Recommendations.queue[0];
+      $scope.currentSong.loaded = false;
+    }, 250);
+
+    /*
+    Recommendations.playCurrentSong().then(function() {
+      $scope.currentSong.loaded = true;
+    });
+    */
+  };
+
+  $scope.nextAlbumImg = function() {
+    if (Recommendations.queue.length > 1) {
+      return Recommendations.queue[1].image_large;
+    }
+
+    return '';
+  }
+
+})
+
+
+/*
+Controller for the favorites page
+*/
+.controller('FavoritesCtrl', function($scope, User) {
+  $scope.favorites = User.favorites;
+  $scope.removeSong = function(song, index) {
+    User.removeSongFromFavorites(song, index);
+  };
+
+})
+
+
+/*
+Controller for our tab bar
+*/
+.controller('TabsCtrl', function($scope) {
+  $scope.enteringFavorites = function() {
+    Recommendations.haltAudio();
+  };
+
+  $scope.leavingFavorites = function() {
+    Recommendations.init();
+  };
+});
---
+++ ionic-course/www/js/services.js
@@ -0,0 +1,77 @@
+angular.module('songhop.services', [])
+  .factory('User', function() {
+
+    var o = {
+      favorites: []
+    };
+
+    o.addSongToFavorites = function(song) {
+      if (!song) return false;
+
+      o.favorites.unshift(song);
+    }
+
+    o.removeSongFromFavorites = function(song, index) {
+      if (!song) return false;
+
+      o.favorites.splice(index, 1);
+    }
+
+    return o;
+  })
+  .factory('Recommendations', function($http, SERVER, $q) {
+
+    var media;
+
+    var o = {
+      queue: []
+    };
+
+    o.init = function() {
+      if (o.queue.length === 0) {
+        return o.getNextSongs();
+
+      } else {
+        return o.playCurrentSong();
+      }
+    };
+
+    o.getNextSongs = function() {
+      return $http({
+        method: 'GET',
+        url: SERVER.url + '/recommendations'
+      }).success(function(data){
+        o.queue = o.queue.concat(data);
+      });
+    };
+
+    o.nextSong = function() {
+      o.queue.shift();
+
+      o.haltAudio();
+
+      if (o.queue.length <= 3) {
+        o.getNextSongs();
+      }
+    };
+
+    o.playCurrentSong = function() {
+      var defer = $q.defer();
+
+      media = new Audio(o.queue[0].preview_url);
+
+      media.addEventListener("loadeddata", function() {
+        defer.resolve();
+      });
+
+      media.play();
+
+      return defer.promise;
+    };
+
+    o.haltAudio = function() {
+      if (media) media.pause();
+    }
+
+    return o;
+  })
